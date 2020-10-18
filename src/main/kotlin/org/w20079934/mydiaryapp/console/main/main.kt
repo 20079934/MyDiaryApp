3abfc1332d78382160c7d0b4fe000426dd4edd36
20079934 <20079934@mail.wit.ie>
2020-10-18 20:56:56 +0100

    Reduced warnings intellij gave

--- src/main/kotlin/org/w20079934/mydiaryapp/console/main/main.kt
+++ src/main/kotlin/org/w20079934/mydiaryapp/console/main/main.kt
@@ -1,65 +1,64 @@
 package org.w20079934.mydiaryapp.console.main
 
-var diaryName = "Mia"
+var diaryName = "World"
 
 fun main()
 {
-    var option = 0;
+    var option: Int
     do
     {
-        option = menu();
+        option = menu()
         when(option)
         {
-            1 -> addDiaryEntry();
-            2 -> readDiaryEntry();
-            3 -> updateDiaryEntry();
-            4 -> deleteDiaryEntry();
-            5 -> changeName();
+            1 -> addDiaryEntry()
+            2 -> readDiaryEntry()
+            3 -> updateDiaryEntry()
+            4 -> deleteDiaryEntry()
+            5 -> changeName()
         }
     }while (option!=0)
 }
 
 fun menu() : Int
 {
-    var input : String? = null;
-    println("My $diaryName");
-    println();
-    println("1 - Add a diary entry");
-    println("2 - Read a previous entry");
-    println("3 - Update an entry");
-    println("4 - Remove an entry");
-    println("5 - Change diary name");
-    println("------");
-    println("0 - Exit");
-    println();
-    print("Select an option:");
-    input = readLine()!!;
-    return if (!input.isNullOrEmpty() && input.toIntOrNull() != null) input.toInt() else -9;
+    println("Hello $diaryName")
+    println()
+    println("1 - Add a diary entry")
+    println("2 - Read a previous entry")
+    println("3 - Update an entry")
+    println("4 - Remove an entry")
+    println("5 - Change diary name")
+    println("------")
+    println("0 - Exit")
+    println()
+    print("Select an option:")
+    val input : String? = readLine()!!
+    return if (!input.isNullOrEmpty() && input.toIntOrNull() != null) input.toInt() else -9
 }
 
 fun addDiaryEntry()
 {
-    println("C");
+    println("C")
 }
 
 fun readDiaryEntry()
 {
-    println("R");
+    println("R")
 }
 
 fun updateDiaryEntry()
 {
-    println("U");
+    println("U")
 }
 
 fun deleteDiaryEntry()
 {
-    println("D");
+    println("D")
 }
 
 fun changeName()
 {
-    print("Current name is [$diaryName], what name to change it to: ");
-    diaryName = readLine()!!;
+    print("Current name is [$diaryName], what name to change it to: ")
+    diaryName = readLine()!!
 }
 
